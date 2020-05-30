510eaf2410e768ca617b77f648f8f18a4a4e2bbe
ramana16 <ramana16@gmail.com>
2020-05-30 21:20:12 +0530

    s9

--- EVA4S9/config.py
+++ EVA4S9/config.py
@@ -8,19 +8,25 @@ class ModelConfig(object):
 		self.batch_size_cuda = 64
 		self.batch_size_cpu = 64	
 		self.num_workers = 4
-		self.epochs = 50
+		# Regularization
 		self.dropout = 0.15
-		self.lr = 0.001
-		self.l1_decay = 1e-6
+		self.l1_decay = 3e-6
 		self.l2_decay = 1e-3
+		# Using OneCycleLR
+		self.lr = 0.1
+		self.max_lr = 0.01
+		self.momentum = 0.9
+		self.epochs = 35
+
 
 	def print_config(self):
 		print("Model Parameters:")
 		pprint.pprint(vars(self), indent=2)
 
-	def test_config():
-		args = ModelConfig()
-		args.print_config()
 
-	if __name__ == '__main__':
-		test_config()
+def test_config():
+	args = ModelConfig()
+	args.print_config()
+
+if __name__ == '__main__':
+	test_config()
