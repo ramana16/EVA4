1afb9e70551113e28dc53e583e741143cc1eedfb
ramana16 <ramana16@gmail.com>
2020-06-10 20:56:19 +0530

    s10

--- EVA4S9/results.py
+++ EVA4S9/results.py
@@ -1,27 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_misclassified_images(img_data, classes, img_name):
+from utilities import denormalize
+
+def plot_images(img_data, classes, img_name):
   figure = plt.figure(figsize=(10, 10))
   
   num_of_images = len(img_data)
   for index in range(1, num_of_images + 1):
-      img = img_data[index-1]["img"] / 2 + 0.5     # unnormalize
+      img = denormalize(img_data[index-1]["img"])  # unnormalize
       plt.subplot(5, 5, index)
       plt.axis('off')
-      plt.imshow(np.transpose(img, (1, 2, 0)))
+      plt.imshow(np.transpose(img.cpu().numpy(), (1, 2, 0)))
       plt.title("Predicted: %s\nActual: %s" % (classes[img_data[index-1]["pred"]], classes[img_data[index-1]["target"]]))
   
   plt.tight_layout()
   plt.savefig(img_name)
 
 def plot_graph(data, metric):
-    fig = plt.figure(figsize=(7, 7))
+    fig, ax = plt.subplots()
+
+    for sub_metric in data.keys():
+      ax.plot(data[sub_metric], label=sub_metric)
     
-    plt.title(f'Validation %s' % (metric))
+    plt.title(f'Change in %s' % (metric))
     plt.xlabel('Epoch')
     plt.ylabel(metric)
-    plt.plot(data)
-    plt.show()
     
-    fig.savefig(f'val_%s_change.png' % (metric.lower()))
+    ax.legend()
+    plt.show()
+
+    fig.savefig(f'%s_change.png' % (metric.lower()))
